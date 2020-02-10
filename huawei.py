7f497bad82501abe99f9af128cc9313ceef64243
谢致邦 (XIE Zhibang) <yeking@red54.com>
2020-02-10 17:26:13 +0800

    Update huawei.py

--- huawei.py
+++ huawei.py
@@ -2,22 +2,19 @@ import requests
 import re
 import html
 
-url = 'https://www.vmall.com/list-36-%s-1-1'
-vmall = 'https://www.vmall.com'
+url = 'https://openapi.vmall.com/mcp/queryPrd?lang=zh-CN&country=CN&portal=1&keyword=36&pageSize=200&searchSortField=1&searchSortType=asc&searchFlag=1'
+vmall = 'https://www.vmall.com/product/%s.html'
 
-for i in range(1, 4):
-	curl = url % i
-	r = requests.get(curl)
-	phones = re.findall('<p class="p-img"><a target="_blank" href="(.*?)"', r.text)
-	for x in phones:
-		purl = vmall + x
-		pr = requests.get(purl)
-		text = html.unescape(pr.text)
-		if '红外' in text:
-			print(purl)
-			title = re.findall('title>(.*)</title', text)[0]
-			print(title)
-			ir = re.findall('(.*红外.*)', text)
-			print(ir)
-			print()
-
+r = requests.get(url)
+phones = re.findall('productId":(.*?),', r.text)
+for x in phones:
+    purl = vmall % x
+    pr = requests.get(purl)
+    text = html.unescape(pr.text)
+    if '红外' in text:
+        print(purl)
+        title = re.findall('title>(.*)</title', text)[0]
+        print(title)
+        ir = re.findall('(.*红外.*)', text)
+        print(ir)
+        print()
